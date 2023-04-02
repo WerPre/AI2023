ed7af0bdb1a5b274358f8ba8968532797f0acc19
WerPre <73884450+WerPre@users.noreply.github.com>
2023-04-02 23:11:14 +0200

    SA to test

--- SA/alghotytm1/alghotytm1/Program.cs
+++ SA/alghotytm1/alghotytm1/Program.cs
@@ -1,25 +1,49 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-float P(float fs, float fs_next, float T)
+
+float NextFloat(float min, float max)
+{
+    Random random = new Random();
+    double val = (random.NextDouble() * (max - min) + min);
+    return (float)val;
+}
+
+float P(float fs, float fs_next, float T, float min, float max)
 {
     double delta = fs_next - fs;
     if (delta <= 0) return 1;
-    else return (float)Math.Exp(delta/T);
+    else return (float)Math.Exp(-delta / T);
 }
-float SA(Func<float> f,float[] S, float s0, bool end_criterion)
-{
-    float best_s = s0;
-    float s_i = s0;
-    int i = 0;
-    float T0 = 1;
-    float s_next = 0;
 
-    while (end_criterion)
+float randomFromArea(float s, float delta, float min, float max)
+{
+    float result = NextFloat(s - delta, s + delta);
+    if (result < min) return min;
+    if (result > max) return max;
+    return result;
+}
+
+float SA(Func<float, float> f, float a, float b, float s0)
+{
+    float s_best = s0;
+    float s_current = s0;
+    float T = 0.99f;
+    float Tmin = 0.00001f;
+    float s_next;
+    float cooling = 0.88f;
+    Random random= new Random();
+
+    while (T > Tmin)
     {
-        
+        for (int i = 0; i < Math.Floor(T); i++)
+        {
+            s_next = randomFromArea(s_current, Math.Abs(a - b) / 1000, a, b);
+            float chance = P(f(s_current), f(s_next), T, a, b);
+            if (chance == 1 || chance > random.NextDouble()) s_best = s_next;
+        }
+        T *= cooling;
     }
 
-    return best_s;
-} 
+    return s_best;
+}
 
-Console.WriteLine("Hello, World!");
