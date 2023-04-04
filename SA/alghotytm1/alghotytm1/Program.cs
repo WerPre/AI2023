8f9ad4bb2a47e735586480caea7832eea8d2c30e
WerPre <73884450+WerPre@users.noreply.github.com>
2023-04-04 11:38:22 +0200

    erasing error from misunderstanding of pseudocode

--- SA/alghotytm1/alghotytm1/Program.cs
+++ SA/alghotytm1/alghotytm1/Program.cs
@@ -1,44 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-float NextFloat(float min, float max)
+double NextDob(double min, double max)
 {
     Random random = new Random();
     double val = (random.NextDouble() * (max - min) + min);
-    return (float)val;
+    return (double)val;
 }
 
-float P(float fs, float fs_next, float T, float min, float max)
+double P(double fs, double fs_next, double T, double min, double max)
 {
     double delta = fs_next - fs;
-    if (delta <= 0) return 1;
-    else return (float)Math.Exp(-delta / T);
+    if (delta < 0) return 1;
+    else return (double)Math.Exp(-delta / T);
 }
 
-float randomFromArea(float s, float delta, float min, float max)
+double randomFromArea(double s, double delta, double min, double max)
 {
-    float result = NextFloat(s - delta, s + delta);
+    double result = NextDob(s - delta, s + delta);
     if (result < min) return min;
     if (result > max) return max;
     return result;
 }
 
-float SA(Func<float, float> f, float a, float b, float s0)
+double SA(Func<double, double> f, double a, double b, double s0)
 {
-    float s_best = s0;
-    float s_current = s0;
-    float T = 1000;
-    float Tmin = 0.0001f;
-    float s_next;
-    float cooling = 0.6f;
+    double s_best = s0;
+    double T = 100;
+    double Tmin = 0.001;
+    double s_next;
+    double cooling = 0.96;
     Random random= new Random();
 
     while (T > Tmin)
     {
-        for (int i = 0; i < 100; i++)
+        for (int i = 0; i <= 100; i++)
         {
-            s_next = randomFromArea(s_current, Math.Abs(a - b)/3, a, b);
-            float chance = P(f(s_current), f(s_next), T, a, b);
+            s_next = randomFromArea(s_best, Math.Abs(a - b)/3, a, b);
+            double chance = P(f(s_best), f(s_next), T, a, b);
             if (chance == 1 || chance > random.NextDouble()) s_best = s_next;
         }
         T *= cooling;
@@ -48,11 +47,11 @@ float SA(Func<float, float> f, float a, float b, float s0)
 }
 
 
-float f(float s)
+double f(double s)
 {
     return s * s;
 }
 
-float result = SA(f, -10, 10, 6);
+double result = SA(f, -10, 10, 10);
 Console.WriteLine("Result: " + result);
 
